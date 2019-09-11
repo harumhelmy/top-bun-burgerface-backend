2ddcc4b4fb67f2ecfa1772771f1c91f64e150798
Harum Helmy <harumendhah@gmail.com>
2019-09-11 10:04:14 -0400

    changed some readme stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Avatar.destroy_all
+Player.destroy_all
+
+tommy = Avatar.create(name: "tommy", img_url: "https://cdn.newsapi.com.au/image/v1/52db7aede2d34ad2a1bda9f407458bfd?width=650")
+iceman = Avatar.create(name: "iceman", img_url: "https://cdn3.movieweb.com/i/article/VZpZarZfYgMAW9fe2Q21RUGY8qVNpw/798:50/Top-Gun-2-Val-Kilmer-Iceman-Return-Volley.jpg")
+
+harum = Player.create(name: "Harum", avatar_id: iceman.id)
+robby = Player.create(name: "TRob", avatar_id: tommy.id)
